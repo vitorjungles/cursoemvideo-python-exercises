0e9eb5bd1c741d78fa5f9a3068314ae4a6db31e3
Vítor Jungles <vitorcamargojungles@gmail.com>
2020-12-30 20:51:05 -0300

    exercicios

--- Python-exercises/ex007.py
+++ Python-exercises/ex007.py
@@ -2,7 +2,6 @@ nt1 = float(input('Nota 1: '))
 nt2 = float(input('Nota 2: '))
 med = (nt1+nt2)/2
 print('A média entre a nota {} e a nota {} é: {:.1f}. '.format(nt1, nt2, med))
-
 if med >= 6:
     print('Você está aprovado! :) ')
 else:
