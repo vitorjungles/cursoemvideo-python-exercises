30d3221a8a666af800167c4487348d0e24c061c9
Vítor Jungles <vitorcamargojungles@gmail.com>
2021-01-09 17:55:54 -0300

    exercicios

--- Python-exercises/ex083.py
+++ Python-exercises/ex083.py
@@ -1,5 +1,25 @@
 li = [str(input('Digite a expressão: '))]
-if li[0].count('(') == li[0].count(')'):
+lif = li[0].strip()
+posia = []
+posif = []
+v = bool
+for pos, c in enumerate(lif):
+    if lif[pos] == '(':
+        posia.append(pos)
+    if lif[pos] == ')':
+        posif.append(pos)
+if len(posia) != len(posif):
+    print('Sua expressão está errada.')
+    v = False
+else:
+    pos = 0
+    for c in range(len(posia)):
+        if posia[pos] < posif[pos]:
+            v = True
+        else:
+            v = False
+print(posia, posif)
+if v == True:
     print('Sua expressão está válida.')
 else:
     print('Sua expressão está errada.')
