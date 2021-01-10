98a6c6a0a30651b43e732f9fc325f6b3ff77356e
Vítor Jungles <vitorcamargojungles@gmail.com>
2021-01-10 14:51:05 -0300

    exercicios

--- exercises/ex084.py
+++ exercises/ex084.py
@@ -1,16 +1,28 @@
 li = []
 p = []
-cont = 0
+maior = menor = 0.0
 while True:
     li.append(str(input('Nome: ')))
     li.append(float(input('Peso: ')))
     p.append(li[:])
-    r = ' '
-    cont += 1
     li.clear()
+    r = ' '
     while r not in 'SN':
         r = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
     if r == 'N':
         break
 print('-='*30)
 print(f'Ao todo, você cadastrou {len(p)} pessoas.')
+for c in range(0, len(p)):
+    if c == 0 or p[c][1] > maior:
+        maior = p[c][1]
+    if c == 0 or p[c][1] < menor:
+        menor = p[c][1]
+print(f'O maior peso foi de {maior}Kg. Peso de ', end='')
+for c in range(0, len(p)):
+    if p[c][1] == maior:
+        print(p[c][0], end=', ')
+print(f'\nO menor peso foi de {menor}Kg. Peso de ', end='')
+for c in range(0, len(p)):
+    if p[c][1] == menor:
+        print(p[c][0], end=', ')
